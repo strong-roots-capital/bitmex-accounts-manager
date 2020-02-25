b19df4ff150f7b534c37fb6994f4fe255c34241a
Eric Crosson <eric.s.crosson@utexas.edu>
2020-02-24 18:00:37 -0600

    chore: remove unused local-words

--- src/accounts.ts
+++ src/accounts.ts
@@ -13,19 +13,19 @@ const AccountShape = t.type({
     apiKeyId: t.string,
     apiKeySecret: t.string
 })
-export type Account = t.TypeOf<typeof AccountShape>;
+export type Account = t.TypeOf<typeof AccountShape>
 
-const NameShape = t.string;
-export type Name = t.TypeOf<typeof NameShape>;
+const NameShape = t.string
+export type Name = t.TypeOf<typeof NameShape>
 
 const AccountsShape = t.record(t.string, AccountShape)
-export type Accounts = t.TypeOf<typeof AccountsShape>;
+export type Accounts = t.TypeOf<typeof AccountsShape>
 
 export function parseAccounts(): Accounts {
-
     const secret = 'accounts'
 
-    const secretValue = secrets.getSync(secret)
+    const secretValue = secrets
+        .getSync(secret)
         .toEither(`${secret} environment variable not defined`)
         .chain(safeParseJson)
         .mapLeft(trace(debug.env))
@@ -34,15 +34,16 @@ export function parseAccounts(): Accounts {
     return pipe(
         right(secretValue),
         chain(AccountsShape.decode),
-        bimap(
-            (error) => {
-                console.error(`Error: unable to parse accounts from ${JSON.stringify(secretValue)}`)
-                return error
-            },
-            trace(debug.env, `Using accounts`)
-        ),
+        bimap(error => {
+            console.error(
+                `Error: unable to parse accounts from ${JSON.stringify(
+                    secretValue
+                )}`
+            )
+            return error
+        }, trace(debug.env, `Using accounts`)),
         fold(() => Object.create(null), id)
     )
 }
 
-//  LocalWords:  apiKeyId apiKeySecret bam secretValue
+//  LocalWords:  bam secretValue
