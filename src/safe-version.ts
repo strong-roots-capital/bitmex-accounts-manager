34112039d072b9c3933d2f2955d45a6f8ef19f83
Eric Crosson <eric.s.crosson@utexas.edu>
2020-02-24 18:21:16 -0600

    chore: appease eslint

--- src/safe-version.ts
+++ src/safe-version.ts
@@ -2,9 +2,12 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 function isVersionPresent(): boolean {
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
     return fs.existsSync(path.join(__dirname, 'version.ts'))
 }
 
 export const version: string = isVersionPresent()
     ? require('./version').version
     : '0.0.0'
+
+//  LocalWords:  fs
